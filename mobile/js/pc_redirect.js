4565f11460a7489d0d067efc692950f3d793cd18
joysm <joysemi113@gmail.com>
2026-02-20 16:57:11 +0900

    반응형 수정

--- mobile/js/pc_redirect.js
+++ mobile/js/pc_redirect.js
@@ -1,7 +1,7 @@
-// // PC 리다이렉션
-// window.addEventListener('resize',()=>{
-//     let deviceWidth=window.innerWidth;        
-//     if(deviceWidth>768){          
-//         window.location.href="http://mygumi8113.dothome.co.kr/snd_kindergarten"        
-//     } 
-// })
+// PC 리다이렉션
+window.addEventListener('resize',()=>{
+    let deviceWidth=window.innerWidth;        
+    if(deviceWidth>768){          
+        window.location.href="http://mygumi8113.dothome.co.kr/snd_kindergarten"        
+    } 
+})
